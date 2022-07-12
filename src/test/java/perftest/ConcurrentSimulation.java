71ca16c860c9c65165f43df2002936c463bff542
Andrzej Ludwikowski <andrzej.ludwikowski@softwaremill.com>
2022-07-12 10:26:56 +0200

    concurrent sim updated to use envs

--- src/test/java/perftest/ConcurrentSimulation.java
+++ src/test/java/perftest/ConcurrentSimulation.java
@@ -2,21 +2,24 @@ package perftest;
 
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.vavr.collection.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.UUID;
 
-import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
 import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.http.HttpDsl.http;
 
 public class ConcurrentSimulation extends BasicSimulation {
 
-    static int howManyShows = 100;
-    private static List<String> showIds = List.range(0, howManyShows)
+    private static final Logger log = LoggerFactory.getLogger(ConcurrentSimulation.class);
+
+    private int howManyShows = usersPerSec * duringSec;
+    private List<String> showIds = List.range(0, howManyShows)
             .map(__ -> UUID.randomUUID().toString());
 
 
@@ -42,8 +45,21 @@ public class ConcurrentSimulation extends BasicSimulation {
                     .body(reserveSeatPayload));
 
     {
-        setUp(createShows.injectOpen(atOnceUsers(howManyShows)).andThen(
-                reserveSeats.injectOpen(constantUsersPerSec(10).during(60))))
+        log.info("Configuration: " + capacityLoadTesting);
+        setUp(createShows.injectOpen(constantUsersPerSec(1000).during(howManyShows / 1000)).andThen(
+                reserveSeats.injectOpen(constantUsersPerSec(usersPerSec).during(duringSec))))
                 .protocols(httpProtocol);
+
+//        if (capacityLoadTesting.enabled) {
+//            setUp(simpleScenario.injectOpen(incrementUsersPerSec(capacityLoadTesting.step)
+//                    .times(capacityLoadTesting.times)
+//                    .eachLevelLasting(capacityLoadTesting.levelLastingSec)
+//                    .separatedByRampsLasting(20)
+//                    .startingFrom(capacityLoadTesting.from)))
+//                    .protocols(httpProtocol);
+//        } else {
+//            setUp(simpleScenario.injectOpen(rampUsersPerSec(1).to(usersPerSec).during(15), constantUsersPerSec(usersPerSec).during(duringSec))
+//                    .protocols(httpProtocol));
+//        }
     }
 }
